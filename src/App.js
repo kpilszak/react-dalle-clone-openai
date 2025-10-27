1783289dd7996172db40519824fccd22d2bee066
kpilszak <klaudia.pilszak@gmail.com>
2025-10-27 05:58:31 -0400

    Add input container

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 const App = () => {
+  const surpriseOptions = [
+    'A blue ostrich eating melon',
+    'A matisse style shark on the telephone',
+    'A pineapple sunbathing on an island'
+  ]
+
   return (
     <div className="app">
-      <section class="search-section"></section>
+      <section class="search-section">
+        <p>Start with a detailed description 
+          <span className="surprise">Surprise me</span>
+        </p>
+        <div className="input-container">
+          <input 
+            placeholder="An impressionist oil painting of a sunflower in a purple vase..."
+          />
+          <button>Generate</button>
+        </div>
+      </section>
       <section class="image-section"></section>
     </div>
   )
