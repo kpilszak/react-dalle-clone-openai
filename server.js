42a7d2bbb3b2672e1a0a0ca6843072e4ed827de4
kpilszak <klaudia.pilszak@gmail.com>
2025-10-28 09:48:31 -0400

    Connect to OpenAI

--- server.js
+++ server.js
@@ -6,4 +6,24 @@ app.use(cors())
 app.use(express.json())
 require('dotenv').config()
 
+const { Configuration, OpenAIApi } = require("openai")
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+})
+const openai = new OpenAIApi(configuration)
+
+app.post('/images', async (req, res) => {
+    try {
+        const response = await openai.createImage({
+            prompt: "A cute baby sea otter",
+            n: 2,
+            size: "1024x1024",
+        })
+        console.log(response)
+        res.send(response.data.data)
+    } catch (error) {
+        console.error(error)
+    }
+})
+
 app.listen(PORT, () => console.log('Your server is running on PORT ' + PORT))
