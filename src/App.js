65dc4dbddb1bfc69f564a077894b929be2b64e77
kpilszak <klaudia.pilszak@gmail.com>
2025-11-03 06:47:19 -0500

    Connect to /images endpoint on frontend

--- src/App.js
+++ src/App.js
@@ -5,6 +5,24 @@ const App = () => {
     'A pineapple sunbathing on an island'
   ]
 
+  const getImages = async () => {
+    try {
+      const options = {
+        method: "POST",
+        body: JSON.stringify({
+          message: "BLUGH"
+        }),
+        headers: {
+          "Content-type": "application/json"
+        }
+      }
+      const response = await fetch('http://localhost:8000/images', options)
+      const data = await response.json()
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   return (
     <div className="app">
       <section class="search-section">
@@ -15,7 +33,7 @@ const App = () => {
           <input 
             placeholder="An impressionist oil painting of a sunflower in a purple vase..."
           />
-          <button>Generate</button>
+          <button onClick={getImages}>Generate</button>
         </div>
       </section>
       <section class="image-section"></section>
